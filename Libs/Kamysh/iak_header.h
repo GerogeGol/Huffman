a6f62ca63de389217dcd58d3e9996105b54c344b
igorkamysh <igorkamysh@yandex.ru>
2022-03-18 10:22:48 +0300

    test kamysh lib

---
+++ Libs/Kamysh/iak_header.h
@@ -0,0 +1,8 @@
+//
+// Created by Игорь Камышенков on 18.03.2022.
+//
+
+#ifndef HUFFMAN_CODE_IAK_HEADER_H
+#define HUFFMAN_CODE_IAK_HEADER_H
+
+#endif //HUFFMAN_CODE_IAK_HEADER_H
---
+++ Libs/Kamysh/iak_source.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Игорь Камышенков on 18.03.2022.
+//
+
+#include <iostream>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+    std::cout << "Hello, world!\n";
+}
