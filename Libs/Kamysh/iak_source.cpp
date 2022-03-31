1cfad4b74d6822c9b0aaa8b663873a75b19ec0ca
igorkamysh <igorkamysh@yandex.ru>
2022-03-31 12:28:42 +0300

    настройка си мэйк лист

--- Libs/Kamysh/iak_source.cpp
+++ Libs/Kamysh/iak_source.cpp
@@ -1,14 +1,6 @@
-#include <iostream>
 #include "iak_header.h"
 
+
 int main(int, char**) {
-    char data[10];
-    strcpy(data, "abcdefg");
-    code_symbol Lib[sizeof(data)];
-    int k = 100;
-    for (int i = 0; i < sizeof(data); i++) {
-        Lib[i].code = k;
-        k++;
-        Lib[i].symbol = data[i];
-    }
+
 }
